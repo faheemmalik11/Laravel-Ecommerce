5424a5184c5f4cedbf4953eae3ce1138f6873962
faheemmalik11 <teamcoldfusion11@gmail.com>
2023-06-19 19:36:48 +0500

    migrations, seeders, relationships, tailwindcss setup completed

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'qazizubair0309@gmail.com',
+            'password' => 'password123', // password
+            'profile_image'=> 'store/images/super_admin.jpg', 
+            'role_id'=>2
+        ]);
+        
+    }
+}
